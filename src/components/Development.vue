9bbe6a9bb23ece4faf2372e6a8b2925e816cfcde
JimJZhu <Jim.Jiaming.Zhu@gmail.com>
2018-04-02 11:00:56 -0400

    Commented out firebase

--- src/components/Development.vue
+++ src/components/Development.vue
@@ -6,23 +6,23 @@
     .tile.is-parent
       .tile.is-child.is-4.box
         router-link(to='/investorbate').title Investorbate
-    .tile.is-parent(v-for="book in books")
-      .tile.is-child.box {{book.author}}
-    .panel.panel-default
-      .panel-heading
-        h3.panel-title Add New Books
-      .panel-body
-        form#form.form-inline(v-on:submit.prevent='addBook')
-          .form-group
-            label(for='bookTitle') Title:
-            input#bookTitle.form-control(type='text', v-model='newBook.title')
-          .form-group
-            label(for='bookAuthor') Author:
-            input#bookAuthor.form-control(type='text', v-model='newBook.author')
-          .form-group
-            label(for='bookUrl') Url:
-            input#bookUrl.form-control(type='text', v-model='newBook.url')
-          input.btn.btn-primary(type='submit', value='Add Book')
+    //- .tile.is-parent(v-for="book in books")
+    //-   .tile.is-child.box {{book.author}}
+    //- .panel.panel-default
+    //-   .panel-heading
+    //-     h3.panel-title Add New Books
+    //-   .panel-body
+    //-     form#form.form-inline(v-on:submit.prevent='addBook')
+    //-       .form-group
+    //-         label(for='bookTitle') Title:
+    //-         input#bookTitle.form-control(type='text', v-model='newBook.title')
+    //-       .form-group
+    //-         label(for='bookAuthor') Author:
+    //-         input#bookAuthor.form-control(type='text', v-model='newBook.author')
+    //-       .form-group
+    //-         label(for='bookUrl') Url:
+    //-         input#bookUrl.form-control(type='text', v-model='newBook.url')
+    //-       input.btn.btn-primary(type='submit', value='Add Book')
 
 </template>
 
